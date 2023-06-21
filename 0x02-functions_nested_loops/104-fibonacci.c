1cc7b6812bcd8f7dcb998868477e05848a42d670
mustaphaabdulbasit35 <mustaphaabdulbasit335@gmail.com>
2023-06-21 07:00:02 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the first 98 fibonacci numbers, starting with
  * 1 and 2, seperated by comma followed by a space.
- * Return: 0 (Success)
+ * Return: Always 0.
  */
 
 int main(void)
