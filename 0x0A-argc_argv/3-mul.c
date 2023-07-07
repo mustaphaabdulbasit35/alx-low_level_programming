136875127c15cafaf66c39e76d464485c3adfea4
mustaphaabdulbasit35 <mustaphaabdulbasit335@gmail.com>
2023-07-07 15:05:18 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,43 +20,42 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
-			while (i < len && f == 0)
-			{
-				if (s[i] == '-')
-					++d;
-
-				if (s[i] >= '0' && s[i] <= '9')
-				{
-					digit = s[i] - '0';
-					if (d % 2)
-						digit = -digit;
-					n = n * 10 + digit;
-					f = 1;
-					if (s[i + 1] < '0' || s[i + 1] > '9')
-						break;
-					f = 0;
-				}
-				i++;
-			}
-			if (f == 0)
-
-				return (0);
-			return (n);
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 success, 1 error
+ * Return: 0 (success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
 
