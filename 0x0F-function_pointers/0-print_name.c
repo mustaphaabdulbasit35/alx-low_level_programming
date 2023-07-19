98b4b4525be4cc71a1390f882ca3e947661f4a0b
mustaphaabdulbasit35 <mustaphaabdulbasit335@gmail.com>
2023-07-19 04:20:33 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: pointer to print the name of the function
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,12 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_PoINTERS
 
-#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
